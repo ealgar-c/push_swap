4a9868e21392f9bb4cfd50fd79ecca07b324d70c
Enrique Algar <enriiquealgar@gmail.com>
2023-06-20 11:18:42 +0200

    fix: :bug: Fixed a bug on the sort3 and added doxygen comments

--- src/smallsort.c
+++ src/smallsort.c
@@ -6,12 +6,18 @@
 /*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 12:38:37 by ealgar-c          #+#    #+#             */
-/*   Updated: 2023/06/19 18:10:22 by ealgar-c         ###   ########.fr       */
+/*   Updated: 2023/06/20 11:18:13 by ealgar-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+/**
+ * @brief Function that sorts 3 numbers in a stack in less
+ * than 3 movements.
+ * 
+ * @param s_a (stack a)
+ */
 void	sort3(t_stack *s_a)
 {
 	if (s_a->numbers[2] > s_a->numbers[1] && s_a->numbers[1] < s_a->numbers[0]
@@ -28,9 +34,9 @@ void	sort3(t_stack *s_a)
 		&& s_a->numbers[1] < s_a->numbers[0]
 		&& s_a->numbers[0] < s_a->numbers[2])
 		ra(s_a);
-	else if (s_a->numbers[2] > s_a->numbers[1]
+	else if (s_a->numbers[2] < s_a->numbers[1]
 		&& s_a->numbers[1] > s_a->numbers[0]
-		&& s_a->numbers[0] < s_a->numbers[2])
+		&& s_a->numbers[0] > s_a->numbers[2])
 	{
 		sa(s_a);
 		ra(s_a);
@@ -41,6 +47,13 @@ void	sort3(t_stack *s_a)
 		rra(s_a);
 }
 
+/**
+ * @brief Function that takes the smallest number of the stack a
+ * and puts it on the stack b.
+ * 
+ * @param s_a (stack a)
+ * @param s_b (stack b)
+ */
 void	smallest_tob(t_stack *s_a, t_stack *s_b)
 {
 	int	i;
@@ -62,11 +75,15 @@ void	smallest_tob(t_stack *s_a, t_stack *s_b)
 	pb(s_a, s_b);
 }
 
+/**
+ * @brief Best way to sort 5 numbers in a stack in less
+ * than 12 movements.
+ * 
+ * @param s_a (stack a)
+ * @param s_b (stack b)
+ */
 void	sort5(t_stack *s_a, t_stack *s_b)
 {
-	int	i;
-
-	i = 0;
 	smallest_tob(s_a, s_b);
 	smallest_tob(s_a, s_b);
 	sort3(s_a);
@@ -74,6 +91,14 @@ void	sort5(t_stack *s_a, t_stack *s_b)
 	pa(s_a, s_b);
 }
 
+/**
+ * @brief Function that takes the cases to sort stacks of
+ * less than 6 numbers. In case of the number of numbers
+ * we have it does something different.
+ * 
+ * @param s_a (stack a)
+ * @param s_b (stack b)
+ */
 void	smallsort(t_stack *s_a, t_stack *s_b)
 {
 	if (s_a->size == 2)
